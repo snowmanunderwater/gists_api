8ad26878632b14232d897b69bc77de14a3ea77a4
fg <vwasdf@gmail.com>
2019-03-23 03:02:16 +0300

    refactor

--- gist_api.py
+++ gist_api.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import argparse
 import json
 import os.path
@@ -14,10 +13,8 @@ with open('TOKEN', 'r') as f:
 BASE_URL = 'https://api.github.com'
 
 
-def str2bool(v):
-    """
-    Function to convert posible bool values to Python bools.
-    """
+def _str2bool(v):
+    # function to convert posible bool values to Python bools.
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
@@ -26,10 +23,11 @@ def str2bool(v):
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
-# ====== Get a single gist ======
-# https://developer.github.com/v3/gists/#get-a-single-gist
 def getSingleGist(GIST_ID):
-    """Print gist content to STDOUT.
+    """
+    https://developer.github.com/v3/gists/#get-a-single-gist
+
+    Print gist content to STDOUT.
 
     Args:
         GIST_ID (str): gist_id.
@@ -65,10 +63,11 @@ def getSingleGist(GIST_ID):
     print('===================')
 
 
-# ====== List a user's gists ======
-# https://developer.github.com/v3/gists/#list-a-users-gists
 def getAllGists(USERNAME):
-    """Print gists information of given user.
+    """
+    https://developer.github.com/v3/gists/#list-a-users-gists
+
+    Print gists information of given user.
 
     Args:
         USERNAME (str): owner of gists.
@@ -109,10 +108,11 @@ def getAllGists(USERNAME):
         count += 1
 
 
-# ====== Create a gist ======
-# https://developer.github.com/v3/gists/#create-a-gist
 def createGist(files, desc, public):
-    """Create gist.
+    """
+    https://developer.github.com/v3/gists/#create-a-gist
+
+    Create gist.
 
     Args:
         files (list): files into gist
@@ -136,35 +136,28 @@ def createGist(files, desc, public):
     json_dict = {
         'files': files_dict,
         'description': desc,
-        'public': str2bool(public)
+        'public': _str2bool(public)
     }
 
-    # convert json_dict to JSON
-    json_data = json.dumps(json_dict)
-    # convert str to bytes (ensure encoding is OK)
-    post_data = json_data.encode('utf-8')
 
-    # we should also say the JSON content type header
+    json_data = json.dumps(json_dict)
+    post_data = json_data.encode('utf-8')
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
         'Authorization': 'token ' + TOKEN
     }
-
-    # now do the request for a url
     req = urllib.request.Request(
         url, data=post_data, headers=headers, method='POST')
 
-    # send the request
     res = urllib.request.urlopen(req)
 
     if res.code == 201:
         print('Gist created!')
 
 
-# ====== Edit a gist ======
-# https://developer.github.com/v3/gists/#edit-a-gist
 def editGist(id, desc, files):
+    # https://developer.github.com/v3/gists/#edit-a-gist
     url = f'https://api.github.com/gists/{id}'
 
     files_dict = {}
@@ -179,37 +172,26 @@ def editGist(id, desc, files):
         'description': desc,
         'files': files_dict,
     }
-
-    # convert json_dict to JSON
     json_data = json.dumps(json_dict)
-    # convert str to bytes (ensure encoding is OK)
     post_data = json_data.encode('utf-8')
-
-    # we should also say the JSON content type header
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
         'Authorization': 'token ' + TOKEN
     }
-
-    # now do the request for a url
     req = urllib.request.Request(
         url, data=post_data, headers=headers, method='PATCH')
-
-    # send the request
     res = urllib.request.urlopen(req)
 
     if res.code == 200:
         print('Gist edited!')
 
 
-# ====== Delete a gist ======
-# https://developer.github.com/v3/gists/#delete-a-gist
 def deleteGist(GIST_ID):
+    # # https://developer.github.com/v3/gists/#delete-a-gist
+
     url = f'{BASE_URL}/gists/{GIST_ID}'
-
     headers = {'Authorization': 'token ' + TOKEN}
-
     req = urllib.request.Request(url, headers=headers, method='DELETE')
 
     try:
@@ -222,10 +204,8 @@ def deleteGist(GIST_ID):
         print('Gist deleted!')
 
 
-# ====== List all public gists ======
-# https://developer.github.com/v3/gists/#list-all-public-gists
 def listPublicGists(since, page, per_page):
-
+    # https://developer.github.com/v3/gists/#list-all-public-gists
     # TODO: Implement since
     # TODO: Add option selection
 
@@ -250,10 +230,9 @@ def listPublicGists(since, page, per_page):
         print('===================')
 
 
-# ====== List starred gists ======
-# https://developer.github.com/v3/gists/#list-starred-gists
-def listStarredGists(since):
 
+def listStarredGists(since):
+    # https://developer.github.com/v3/gists/#list-starred-gists
     # TODO: Implement since
     # TODO: Add option selection
 
@@ -278,10 +257,8 @@ def listStarredGists(since):
         print('===================')
 
 
-# ====== Get a specific revision of a gist ======
-# https://developer.github.com/v3/gists/#get-a-specific-revision-of-a-gist
 def specificRevisionOfAGist(GIST_ID, SHA):
-
+    # https://developer.github.com/v3/gists/#get-a-specific-revision-of-a-gist
     # FIXME: SHA don't work, at to date work like list gist
 
     try:
@@ -295,10 +272,8 @@ def specificRevisionOfAGist(GIST_ID, SHA):
     print(cont)
 
 
-# ====== List gist commits ======
-# https://developer.github.com/v3/gists/#list-gist-commits
 def listGistCommits(GIST_ID):
-
+    # https://developer.github.com/v3/gists/#list-gist-commits
     # TODO: Implement since
     # TODO: Add option selection
 
@@ -331,9 +306,8 @@ def listGistCommits(GIST_ID):
         print('===================')
 
 
-# ====== Star a gist ======
-# https://developer.github.com/v3/gists/#star-a-gist
 def starGist(GIST_ID):
+    # https://developer.github.com/v3/gists/#star-a-gist
     url = f'{BASE_URL}/gists/{GIST_ID}/star'
     headers = {'Authorization': 'token ' + TOKEN, 'Content-Length': 0}
     req = urllib.request.Request(url, headers=headers, method='PUT')
@@ -348,9 +322,8 @@ def starGist(GIST_ID):
         print('Starred!')
 
 
-# ====== Unstar a gist ======
-# https://developer.github.com/v3/gists/#unstar-a-gist
 def unstarGist(GIST_ID):
+    # https://developer.github.com/v3/gists/#unstar-a-gist
     url = f'{BASE_URL}/gists/{GIST_ID}/star'
     headers = {
         'Authorization': 'token ' + TOKEN,
@@ -367,13 +340,11 @@ def unstarGist(GIST_ID):
         print('Unstarred!')
 
 
-# ====== Check if a gist is starred ======
-# https://developer.github.com/v3/gists/#check-if-a-gist-is-starred
+
 def checkGistStarred(GIST_ID):
+    # https://developer.github.com/v3/gists/#check-if-a-gist-is-starred
     url = f'{BASE_URL}/gists/{GIST_ID}/star'
     headers = {
-        # 'Content-Type': 'application/json',
-        # 'Accept': 'application/json',
         'Content-Length': 0,
         'Authorization': 'token ' + TOKEN
     }
@@ -389,9 +360,8 @@ def checkGistStarred(GIST_ID):
         print('Starred!')
 
 
-# ====== Fork a gist ======
-# https://developer.github.com/v3/gists/#fork-a-gist
 def forkGist(GIST_ID):
+    # https://developer.github.com/v3/gists/#fork-a-gist
     url = f'{BASE_URL}/gists/{GIST_ID}/forks'
     headers = {
         'Content-Type': 'application/json',
@@ -411,10 +381,8 @@ def forkGist(GIST_ID):
         print('Gist forked')
 
 
-# ====== List gist forks ======
-# https://developer.github.com/v3/gists/#list-gist-forks
 def listGistForks(GIST_ID):
-
+    # https://developer.github.com/v3/gists/#list-gist-forks
     # TODO: page, per_page
 
     url = f'{BASE_URL}/gists/{GIST_ID}/forks'
@@ -449,119 +417,79 @@ def listGistForks(GIST_ID):
 
 if __name__ == '__main__':
 
-    # TODO: refactor argparse
-
     parser = argparse.ArgumentParser(description='Github Gists CLI')
     parser.add_argument('name', type=str, help='Name of method')
     parser.add_argument('-id', '--gist_id', type=str, help='Gist ID')
     parser.add_argument('-u', '--username', type=str, help='Name of user')
     parser.add_argument('-f', '--files', nargs='*', help='Path to files')
-    parser.add_argument(
-        '-d',
-        '--description',
-        type=str,
-        default='',
-        help='A descriptive name for this gist.')
-    parser.add_argument(
-        '-p',
-        '--public',
-        type=str,
-        default='yes',
-        help='Gist status(public or private). Default is Public')
-    parser.add_argument(
-        '-s',
-        '--since',
-        type=str,
-        help=
-        'This is a timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ. Only gists updated at or after this time are returned.'
-    )
-    parser.add_argument(
-        '-pg', '--page', type=int, default='1', help='Paginator: page')
-    parser.add_argument(
-        '-pp', '--per_page', type=int, default='3', help='Paginator: per page')
-    parser.add_argument(
-        '-sha', '--sha', type=str, default='', help='SHA of gist commit')
+    parser.add_argument('-d',
+                        '--description',
+                        type=str,
+                        default='',
+                        help='A descriptive name for this gist.')
+    parser.add_argument('-p',
+                        '--public',
+                        type=str,
+                        default='yes',
+                        help='Gist status(public or private). Default is Public')
+    parser.add_argument('-s',
+                        '--since',
+                        type=str,
+                        help='This is a timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ.')
+    parser.add_argument('-pg', '--page', type=int, default='1', help='Paginator: page')
+    parser.add_argument('-pp', '--per_page', type=int, default='3', help='Paginator: per page')
+    parser.add_argument('-sha', '--sha', type=str, default='', help='SHA of gist commit')
 
     args = parser.parse_args()
 
-    # ====== Get a single gist ======
-    # https://developer.github.com/v3/gists/#get-a-single-gist
     if args.name == 'gist':
         getSingleGist(args.gist_id)
 
-    # ====== List a user's gists ======
-    # https://developer.github.com/v3/gists/#list-a-users-gists
     if args.name == 'list':
         getAllGists(args.username)
 
-    # ====== Create a gist ======
-    # https://developer.github.com/v3/gists/#create-a-gist
     if args.name == 'create':
         for file in args.files:
             if not os.path.isfile(file):
                 raise Exception(f"{file} IS NOT A FILE")
         createGist(args.files, args.description, args.public)
 
-    # ====== Edit a gist ======
-    # https://developer.github.com/v3/gists/#edit-a-gist
     if args.name == 'edit':
         for file in args.files:
             if not os.path.isfile(file):
                 raise Exception(f"{file} IS NOT A FILE")
         editGist(args.gist_id, args.description, args.files)
 
-    # ====== Delete a gist ======
-    # https://developer.github.com/v3/gists/#delete-a-gist
     if args.name == 'delete':
         deleteGist(args.gist_id)
 
-    # ====== List all public gists ======
-    # https://developer.github.com/v3/gists/#list-all-public-gists
     if args.name == 'lp':
         listPublicGists(args.since, args.page, args.per_page)
 
-    # ====== List starred gists ======
-    # https://developer.github.com/v3/gists/#list-starred-gists
     if args.name == 'starred':
         listStarredGists(args.since)
 
-    # ====== Get a specific revision of a gist ======
-    # https://developer.github.com/v3/gists/#get-a-specific-revision-of-a-gist
     if args.name == 'srg':
         specificRevisionOfAGist(args.gist_id, args.sha)
 
-    # ====== List gist commits ======
-    # https://developer.github.com/v3/gists/#list-gist-commits
     if args.name == 'lgc':
         listGistCommits(args.gist_id)
 
-    # ====== Star a gist ======
-    # https://developer.github.com/v3/gists/#star-a-gist
     if args.name == 'star':
         starGist(args.gist_id)
 
-    # ====== Unstar a gist ======
-    # https://developer.github.com/v3/gists/#unstar-a-gist
     if args.name == 'unstar':
         unstarGist(args.gist_id)
 
-    # ====== Check if a gist is starred ======
-    # https://developer.github.com/v3/gists/#check-if-a-gist-is-starred
     if args.name == 'check':
         checkGistStarred(args.gist_id)
 
-    # ====== Fork a gist ======
-    # https://developer.github.com/v3/gists/#fork-a-gist
     if args.name == 'fork':
         forkGist(args.gist_id)
 
-    # ====== List gist forks ======
-    # https://developer.github.com/v3/gists/#list-gist-forks
     if args.name == 'lgf':
         listGistCommits(args.gist_id)
 
     # FIXME: when create gist or star allready starred gist, this prints
     else:
-        print(
-            f"""gist_api: {args.name} is not a gist_api command. See 'gist_api --help'."""
-        )
+        print(f"gist_api: {args.name} is not a gist_api command. See 'gist_api -h'.")
